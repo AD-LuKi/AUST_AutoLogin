3f4de2a5a1c4bf90c645143834281ee744661483
LuKi <130198592+AD-LuKi@users.noreply.github.com>
2024-05-20 15:13:58 +0800

    Update AutoLogin.py
    delete five unuseful properities in send_data

--- AutoLogin.py
+++ AutoLogin.py
@@ -3,18 +3,7 @@ address = 'http://10.255.0.19/a79.htm'
 
 # DDDDD一项为'学号@后缀'形式
 # 后缀：联通写unicom，电信写aust，移动写cmcc
-send_data = {
-'callback': 'dr1003',
-'DDDDD': '学号@后缀',
-'upass': '校园网密码',
-'0MKKey': '123456',
-'R1': 0,
-'R3': 0,
-'R6': 0,
-'para': 00,
-  # 电信6774，移动8487
-'v': 8843
-}
+send_data = {'callback': 'dr1003', 'DDDDD': '学号@后缀', 'upass': '校园网密码', '0MKKey': '123456'}
 request_header = {
 'Accept': '*/*',
 'Accept-Encoding': 'gzip, deflate',
