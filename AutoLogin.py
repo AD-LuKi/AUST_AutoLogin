53ebd8f2a7ff1a207095e3d8e908d386219028b0
LuKi <130198592+AD-LuKi@users.noreply.github.com>
2024-05-20 15:03:22 +0800

    Update AutoLogin.py
    add three situations of different company of network service to DDDDD's explanation

--- AutoLogin.py
+++ AutoLogin.py
@@ -1,9 +1,12 @@
 import requests as R
 address = 'http://10.255.0.19/a79.htm'
+
+# DDDDD一项为'学号@后缀'形式
+# 后缀：联通写unicom，电信写aust，移动写cmcc
 send_data = {
 'callback': 'dr1003',
-'DDDDD': '此处填学号@unicom',
-'upass': '此处填校园网密码',
+'DDDDD': '学号@后缀',
+'upass': '校园网密码',
 '0MKKey': '123456',
 'R1': 0,
 'R3': 0,
@@ -22,4 +25,6 @@ request_header = {
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 }
 response = R.post(address,send_data,headers=request_header).status_code
+
+# 一般接受返回信息应该是200
 print("返回信息 : {}".format(response))
