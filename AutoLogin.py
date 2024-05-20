3638f8d564c5234e0b8a1a6bb077a4acd4b7b219
LuKi <130198592+AD-LuKi@users.noreply.github.com>
2024-05-20 09:04:32 +0800

    Update AutoLogin.py
    update return message

--- AutoLogin.py
+++ AutoLogin.py
@@ -1,7 +1,5 @@
 import requests as R
-
 address = 'http://10.255.0.19/a79.htm'
-
 send_data = {
 'callback': 'dr1003',
 'DDDDD': '2022304240@unicom',
@@ -13,7 +11,6 @@ send_data = {
 'para': 00,
 'v': 8843
 }
-
 request_header = {
 'Accept': '*/*',
 'Accept-Encoding': 'gzip, deflate',
@@ -24,7 +21,5 @@ request_header = {
 'Referer': 'http://10.255.0.19/a79.htm',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 }
-
 response = R.post(address,send_data,headers=request_header).status_code
-
-print("回应代码{}".format(response))
+print("返回信息 : {}".format(response))
